693d5886ea1b6da1e9a2f60ca48c28b1451ba158
arzateCompany <osvarz.new@gmail.com>
2021-03-25 07:12:51 -0600

    🔧 updated configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        0: "0",
+        "1/2": "50%",
+        "1/3": "",
+        "1/4": "25%",
+        "1/5": "33.333333%",
+        "2/3": "66.666667%",
+        "3/4": "75%",
+        full: "100%",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+    },
   },
   plugins: [],
 };
