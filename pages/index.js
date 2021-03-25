49d73483ab2e9c6ad0f414eaae0408b4b8105968
arzateCompany <osvarz.new@gmail.com>
2021-03-25 08:27:49 -0600

    🔧 tailwind setting changed

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="bg-gray-300 h-screen"></main>
+      <main className="h-screen bg-gradient-to-bl from-black-warm to-orange-bitcoin">
+        <p className="font-inter text-5xl font-bold">inter</p>
+        <p className="font-dm-sans text-5xl font-bold">dm</p>
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        "orange-bitcoin": "#F7931A",
+        "orange-soft": "#FFE9D5",
+        "blue-secondary": "#1A9AF7",
+        "blue-soft": "#E7F5FF",
+        "black-warm": "#201E1C",
+        "black-warm-100": "#282623",
+        "gray-50": "#BABABA",
+        "white-off": "#FAF8F7",
+      },
+      fontFamily: {
+        "dm-sans": ["DM Sans", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
       minWidth: {
         0: "0",
         "1/2": "50%",
